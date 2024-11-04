e00fca1c458f52949f224242a7eb493e40d1b13e
manuK1777 <manuelkrapovickas@gmail.com>
2024-11-04 11:38:21 +0100

    Exercise 1 completed: Createdcomponents: header, home, mapa, calendar and grafics. Set up routing for each component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { FullCalendarComponent } from './components/full-calendar/full-calendar.component';
+import { GraficsComponent } from './components/grafics/grafics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'mapa', component: MapaComponent },
+    { path: 'calendario', component: FullCalendarComponent },
+    { path: 'grafics', component: GraficsComponent },
+    
+];
---
+++ src/app/components/grafics/grafics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafics',
+  standalone: true,
+  imports: [],
+  templateUrl: './grafics.component.html',
+  styleUrl: './grafics.component.scss'
+})
+export class GraficsComponent {
+
+}
